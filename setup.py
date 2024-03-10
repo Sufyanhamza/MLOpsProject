6ade0a50baabb0715c50b11266064e4ee2c1fd0e
sufiyanhamza07 <sufyan.hamza07@gmail.com>
2024-03-10 12:17:22 +0500

    Update files and code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-"""HYPEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,13 +12,31 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+    return requirements
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "MLOpsProject"
+PKG_NAME= "databaseautomation"
+AUTHOR_USER_NAME = "Sufyanhamza"
+AUTHOR_EMAIL = "sufyan.hamza07@gmail.com"
 
 setup(
-    name='DimondPricePrediction',
-    version='0.0.1',
-    author='Sufyanhamza',
-    author_email='sufyan.hamza07@gmail.com',
-    install_requires=["scikit-learn","pandas","numpy"],
-    packages=find_packages()
-)
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements("./requirement_dev.txt"),
+    )
